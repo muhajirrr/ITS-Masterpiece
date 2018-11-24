0a462c9af099d1c52dad2fae46e2b93b8a79995a
haimax <muhajir16@mhs.if.its.ac.id>
2018-11-24 10:27:37 +0700

    add web traffic

--- resources/views/dashboard/components/js.blade.php
+++ resources/views/dashboard/components/js.blade.php
@@ -3,7 +3,6 @@
 <script src="{{ asset('dashboard/js/bootstrap-checkbox-radio.js') }}"></script>
 <script src="{{ asset('dashboard/js/chartist.min.js') }}"></script>
 <script src="{{ asset('dashboard/js/bootstrap-notify.js') }}"></script>
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
 <script src="{{ asset('dashboard/js/paper-dashboard.js') }}"></script>
 {{-- <script src="{{ asset('dashboard/js/demo.js') }}"></script> --}}
 <script>
--- resources/views/dashboard/home/index.blade.php
+++ resources/views/dashboard/home/index.blade.php
@@ -1,3 +1,99 @@
 @extends('dashboard.template')
 
 @section('title', 'Home')
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="card">
+                <div class="header">
+                    <h4 class="title">Traffic</h4>
+                    <p class="category">Detail Web Traffic</p>
+
+                    <div class="content">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div id="chart-1-container"></div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div id="chart-2-container"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('js')
+@php
+    $KEY_FILE_LOCATION = public_path(Storage::url("masterpieceits-8f6807a457b3.json"));
+    $acc = new \Google\Auth\Credentials\ServiceAccountCredentials(['https://www.googleapis.com/auth/analytics.readonly'],$KEY_FILE_LOCATION);
+    $authToken = $acc->fetchAuthToken();
+    $access_token = $authToken["access_token"];
+@endphp 
+<script>
+(function(w,d,s,g,js,fs){
+    g=w.gapi||(w.gapi={});g.analytics={q:[],ready:function(f){this.q.push(f);}};
+    js=d.createElement(s);fs=d.getElementsByTagName(s)[0];
+    js.src='https://apis.google.com/js/platform.js';
+    fs.parentNode.insertBefore(js,fs);js.onload=function(){g.load('analytics');};
+}(window,document,'script'));
+</script>
+
+<script>
+    gapi.analytics.ready(function() {
+        
+      gapi.analytics.auth.authorize({
+        'serverAuth': {
+          'access_token': '{{ $access_token }}'
+        }
+      });
+    
+      var dataChart1 = new gapi.analytics.googleCharts.DataChart({
+        query: {
+          'ids': 'ga:185180615',
+          'start-date': '30daysAgo',
+          'end-date': 'yesterday',
+          'metrics': 'ga:sessions,ga:users',
+          'dimensions': 'ga:date'
+        },
+        chart: {
+          'container': 'chart-1-container',
+          'type': 'LINE',
+          'options': {
+            'width': '100%'
+          }
+        }
+      });
+      dataChart1.execute();
+    
+      var dataChart2 = new gapi.analytics.googleCharts.DataChart({
+        query: {
+          'ids': 'ga:185180615',
+          'start-date': '30daysAgo',
+          'end-date': 'yesterday',
+          'metrics': 'ga:pageviews',
+          'dimensions': 'ga:pagePathLevel1',
+          'sort': '-ga:pageviews',
+          'filters': 'ga:pagePathLevel1!=/',
+          'max-results': 7
+        },
+        chart: {
+          'container': 'chart-2-container',
+          'type': 'PIE',
+          'options': {
+            'width': '100%',
+            'pieHole': 4/9,
+          }
+        }
+      });
+      dataChart2.execute();
+    
+    });
+    </script>
+@endsection
